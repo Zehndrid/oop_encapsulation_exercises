7c8172fb388eb841b0305607e4a9e64a9a1cf738
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-06-02 23:25:33 +0800

    adjusted then add accessor and mutator for color attribute

--- smart_fan_simulator/fan_class.py
+++ smart_fan_simulator/fan_class.py
@@ -11,6 +11,7 @@ class Fan:
 
     def get_speed(self): return self.__speed
     def set_speed(self, speed): self.__speed = speed
-
     def get_on(self): return self.__on
     def set_on(self, on): self.__on = on
+    def get_color(self): return self.__color
+    def set_color(self, color): self.__color = color
