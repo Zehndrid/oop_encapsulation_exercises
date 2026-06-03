eb298fdd0308e79554d6abbb126ba77b5d99c088
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-06-03 19:17:50 +0800

    Add missing setter method for age attribute

--- pet_registration_system/pet_register_simulation.py
+++ pet_registration_system/pet_register_simulation.py
@@ -6,6 +6,7 @@ class Pet:
 
     def set_name(self, name): self.__name = name
     def set_animal_type(self, animal_type): self.__animal_type = animal_type
+    def set_age(self, age): self.__age = age
 
     def get_name(self): return self.__name
     def get_animal_type(self): return self.__animal_type
