fca4407bbfd32dab360188acfe71ef5241f97f44
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-06-03 18:51:54 +0800

    Add accessor methods for all attributes

--- pet_registration_system/pet_register_simulation.py
+++ pet_registration_system/pet_register_simulation.py
@@ -6,3 +6,7 @@ class Pet:
 
     def set_name(self, name): self.__name = name
     def set_animal_type(self, animal_type): self.__animal_type = animal_type
+
+    def get_name(self): return self.__name
+    def get_animal_type(self): return self.__animal_type
+    def get_age(self): return self.__age
