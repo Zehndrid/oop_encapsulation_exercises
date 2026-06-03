df447fa2071d65cc29e1ef08de29f948bcd07c00
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-06-03 16:05:40 +0800

    fixed errors

--- smart_fan_simulator/fan_class.py
+++ smart_fan_simulator/fan_class.py
@@ -16,16 +16,16 @@ class Fan:
     def get_color(self): return self.__color
     def set_color(self, color): self.__color = color
     def get_radius(self): return self.__radius
-    def set_radius(self, radius): self.__radius = float(radius)
 
-    if __name__ == "__main__":
-        fan1 = Fan(speed=Fan.FAST, radius=10.0, color="yellow", on=True)
-        fan2 = Fan(speed=Fan.MEDIUM, radius=5.0, color="blue", on=False)
     def set_radius(self, radius):
         if radius > 0:
             self.__radius = float(radius)
         else:
             self.__radius = 5.0 # Fallback to default
 
-            print(f"Fan 1 -> Speed: {fan1.get_speed()}, Color: {fan1.get_color()}, On: {fan1.get_on()}")
-            print(f"Fan 2 -> Speed: {fan2.get_speed()}, Color: {fan2.get_color()}, On: {fan2.get_on()}")
+if __name__ == "__main__":
+    fan1 =Fan(speed=Fan.FAST, radius=10.0, color="yellow", on=True)
+    fan2 = Fan(speed=Fan.MEDIUM, radius=5.0, color="blue", on=False)
+   
+    print(f"Fan 1 -> Speed: {fan1.get_speed()}, Color: {fan1.get_color()}, On: {fan1.get_on()}")
+    print(f"Fan 2 -> Speed: {fan2.get_speed()}, Color: {fan2.get_color()}, On: {fan2.get_on()}")
