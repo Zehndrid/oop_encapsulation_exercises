17907de3ef330664559bb1dd623a58291b07ad4c
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-06-03 18:31:32 +0800

    fix errors on identation

--- f1_speed_controller/car_class.py
+++ f1_speed_controller/car_class.py
@@ -4,26 +4,33 @@ class Car:
         self.__make = make
         self.__speed = 0
         self.__top_speed = top_speed
+
     def accelerate(self):
         if self.__speed + 5 <= self.__top_speed:
             self.__speed += 5
         else:
             self.__speed = self.__top_speed
+
     def brake(self):
         if self.__speed >= 5:
             self.__speed -= 5
         else:
             self.__speed = 0
+
     def get_speed(self):
         return self.__speed
     
-    if __name__ == "__main__":
-        my_car = Car("2026", "Red Bull Racing RB20", 220)
-        print("--- RB20 Telemetry Start ---")
-        for i in range(5):
-            my_car.accelerate()
-            print(f"Accelerating: {my_car.get_speed()} mph")
-            print("--- Braking Zone ---")
-            for i in range(5):
-                my_car.brake()
-                print(f"Decelerating: {my_car.get_speed()} mph")
+if __name__ == "__main__":
+    my_car = Car("2026", "Red Bull Racing RB20", 220)
+        
+    print("--- RB20 Telemetry Start ---")
+        
+    for i in range(5):
+        my_car.accelerate()
+        print(f"Accelerating: {my_car.get_speed()} mph")
+            
+        print("--- Braking Zone ---")
+           
+    for i in range(5):
+        my_car.brake()
+        print(f"Decelerating: {my_car.get_speed()} mph")
